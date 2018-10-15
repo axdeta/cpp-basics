ef4f0b494a7caa71f2b303afb1bd08619be20efe
Denis.B <den120185@mail.ru>
2018-10-15 21:16:10 +0300

    fix

--- 02-conditionals-loops-1/main.cpp
+++ 02-conditionals-loops-1/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 #include <string>
 
 using namespace std;
 
+int main()
+{
+	const double kEps = 1e-15;
 
-int main() {
-	double a, b, c, x1, x2, dx, f;
-	const double EPS = 1e-15;
-	cout << fixed;
+	double a, b, c, x1, x2, dx;
 	cout << "Enter a -> ";
 	cin >> a;
 	cout << "Enter b -> ";
@@ -22,38 +21,54 @@ int main() {
 	cin >> x2;
 	cout << "Enter dx -> ";
 	cin >> dx;
-	if (dx < EPS) { cout << "Error dx"; }
+
+	if (dx < kEps) cout << "Error dx";
 	else
 	{
-		cout << string(25, '-') << endl;
-		cout << "|     X     |     F     |" << endl;
-		cout << string(25, '-') << endl;
+		cout << string(37, '-') << endl;
+		cout << "|        X        |        F        |" << endl;
+		cout << string(37, '-') << endl;
+
+		cout << fixed;
 		cout.precision(3);
-		while (x1 <= x2) {
-			if ((a < 0) and (abs(c) > EPS))
+
+		double f;
+		while (x1 <= x2)
+		{
+			cout << "|" << setw(11) << x1 << setw(7) << "|";
+
+			if ((a < 0) and (abs(c) > kEps))
 			{
-				f = a * x1*x1 + b * x1 + c;
+				f = a * x1 * x1 + b * x1 + c;
 			}
-			else if ((a > 0) and (abs(c) < EPS) and ((abs(x1 - c)) >= EPS))
+			else if ((a > 0) and (abs(c) < kEps))
 			{
-				f = -a / (x1 - c);
+				if (!(abs(x1 - c) < kEps))
+				{
+					f = -a / (x1 - c);
+				}
+				else
+				{
+					cout << "  division by 0  |" << endl;
+					x1 += dx;
+					continue;
+				}
 			}
 			else
 			{
 				f = a * (x1 + c);
 			}
 
-			if (abs(int(a) & (int(b) | int(c))) > EPS) {
-				cout << "|" << setw(6) << x1 << setw(6) << "|";
-				cout << setw(6) << f << setw(6) << "|" << endl;
-			}
-			else {
-				cout << "|" << setw(6) << x1 << setw(6) << "|";
-				cout << setw(6) << int(f) << setw(6) << "|" << endl;
-			}
+			cout << setw(11);
+			if ((int(a) & (int(b) | int(c))) != 0)
+				cout << f;
+			else
+				cout << int(f);
+			cout << setw(7) << "|" << endl;
+
 			x1 += dx;
 		}
-		cout << string(25, '-') << endl;
+		cout << string(37, '-') << endl;
 	}
 	return 0;
 }
